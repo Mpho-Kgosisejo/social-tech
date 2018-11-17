7f1bcd8061c192abd79a00d2b432abd5e19f86bf
Melissa NAIDOO <mnaidoo@e5r1p3.wethinkcode.co.za>
2018-11-17 06:15:13 +0200

    done done

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,9 +1,6 @@
-import Router from "next/router"
 import Layout from "../components/Layouts/Layout"
-import ContextAPI from "../src/config/ContextAPI"
 import React from 'react'
-import { Divider, Image, Card, Button, Icon, Header } from 'semantic-ui-react'
-import axios from 'axios';
+import { Card, Button, Header, Divider, Container } from 'semantic-ui-react'
 import "../static/css/gallery.css";
 import api from '../src/providers/APIRequest';
 import { isEmptyObj } from "../src/utils/Objs";
@@ -12,101 +9,113 @@ import { isEmptyObj } from "../src/utils/Objs";
 //const API = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=4568877052.1677ed0.a60877d3a14849cbaf47fb1acb2a88ad';
 
 class Gallery extends React.Component {
-  constructor() {
-    super()
+    constructor() {
+        super()
 
-    this.state = {
-      data: [],
-      mainUrl: "",
-      imageIndex: 0
+        this.state = {
+            data: [],
+            imageIndex: 0,
+            size: {}
+        }
+        this.mainImageRef = React.createRef()
     }
-    this.mainImageRef = React.createRef()
-  }
 
-  getData = async () => {
-    const res = await api.gallery.getInstaImgs()
-
-    if (res.status === 200) {
-      const { data } = res.data
-      this.setState({ data, mainUrl: data[0].images.low_resolution.url });
-    } else {
-      res.status(404).send('Bad Request')
+    s = (el) => {
+        if (el){
+            this.setState({size: el.getBoundingClientRect()})
+        }
     }
-  }
 
-  componentDidMount() {
-    this.getData()
-  }
+    getData = async () => {
+        const res = await api.gallery.getInstaImgs()
 
-  myIncludes = (str) => {
-    return (str.includes(".jpg") || str.includes(".JPG") || str.includes(".PNG") || str.includes(".png"))
-  }
+        if (res.status === 200) {
+            const { data } = res.data
+            this.setState({ data });
+        } else {
+            res.status(404).send('Bad Request')
+        }
+    }
 
-  autoScroll = () => {
-    window.scrollTo({
-      top: this.mainImageRef,
-      behavior: "smooth"
-    })
-  }
+    componentDidMount() {
+        this.getData()
+    }
 
-  nextImage = () => {
-    this.setState({
-      imageIndex: this.state.imageIndex + 1
-    })
-  }
-  prevImage = () => {
-    this.setState({
-      imageIndex: this.state.imageIndex - 1
-    })
-  }
+    myIncludes = (str) => {
+        return (str.includes(".jpg") || str.includes(".JPG") || str.includes(".PNG") || str.includes(".png"))
+    }
 
-  onSelectImage = (url, indx) => this.setState({ mainUrl: url, imageIndex: indx })
+    autoScroll = () => {
+        window.scrollTo({
+            top: this.state.size.top,
+            behavior: "smooth"
+        })
+    }
+
+    nextImage = () => {
+        this.setState({
+            imageIndex: this.state.imageIndex + 1
+        })
+    }
+    prevImage = () => {
+        this.setState({
+            imageIndex: this.state.imageIndex - 1
+        })
+    }
+
+    onSelectImage = (indx) => this.setState({ imageIndex: indx })
 
 
-  render() {
-    const { data, imageIndex, mainUrl } = this.state
+    render() {
+        const { data, imageIndex } = this.state
 
-    return (
-      <>
-        <div style={{ marginTop: "75px" }}>
-          <Header as='h2' icon textAlign='center'>
-            <Icon name='camera' circular />
-            <Header.Content className="font_header" >Our Gallery</Header.Content>
-          </Header>
-        </div>
-        {!isEmptyObj(data) ?
-          <div className="image_height">
-            <div className="ui container slides">
-            {imageIndex > 0 && <i className="big angle arrow left icon arrow-left" onClick={() => { this.prevImage() }} />}
-            {imageIndex < data.length - 1 && <i className="big angle arrow right icon arrow_right" onClick={() => { this.nextImage() }} />}
-              <div className="gallery-card img_height" style={{ background: `url(${data[imageIndex].images.standard_resolution.url})` }}>
-                <hidden ref={this.mainImageRef} />
-              </div>
-            </div>
-          </div>
-          : ""}
-        <Layout title="Gallery">
-          {!isEmptyObj(data) ? <>
-            <Card.Group  doubling itemsPerRow={3} stackable>
-              {data.map(el => {
-                return (
-                  <Card
-                    onClick={() => {
-                      this.onSelectImage(el.images.low_resolution.url, data.indexOf(el))
-                      this.autoScroll()
-                    }}>
-                    <div className="gallery-card gallery-height" style={{background: `url(${el.images.low_resolution.url})` }}></div>
-                    <Button className="insta-butt" onClick={() =>{window.open(data[imageIndex].link)}}>
-                    <i>Instagram</i>
-                    </Button>
-                  </Card>
-                )
-              })}
-            </Card.Group>
-          </> : ""}
-        </Layout>
-      </>
-    )
-  }
+        return (
+            <>
+                <Container style={{ marginTop: "75px" }}>
+                    <Header as='h2' textAlign='center'>
+                        Our Gallery
+                    </Header>
+                    <Divider />
+                </Container>
+
+                {!isEmptyObj(data) ?
+                    <div className="image_height">
+                        <div className="ui container slides">
+                            {imageIndex > 0 && <div className="icon-container left"><i className="big angle arrow left icon arrow-left" onClick={() => { this.prevImage() }} /></div>}
+
+                            <div className="gallery-card img_height" style={{ backgroundImage: `url(${data[imageIndex].images.standard_resolution.url})` }}>
+                                <i ref={this.s} />
+                            </div>
+
+                            {imageIndex < data.length - 1 && <div className="icon-container right"><i className="big angle arrow right icon arrow_right" onClick={() => { this.nextImage() }} /></div>}
+                        </div>
+                    </div>
+                    : null
+                }
+
+                <Layout title="Gallery">
+                    {!isEmptyObj(data) ? <>
+                        <Card.Group doubling itemsPerRow={3} stackable>
+                            {data.map(el => {
+                                return (
+                                    <Card key={el.id}
+                                        onClick={() => {
+                                            this.onSelectImage(data.indexOf(el))
+                                            this.autoScroll()
+                                        }}>
+                                        <div className="gallery-card gallery-height" style={{ background: `url(${el.images.low_resolution.url})` }}></div>
+                                        <Button className="insta-butt" onClick={() => { window.open(data[imageIndex].link) }}>
+                                            <i>Instagram</i>
+                                        </Button>
+                                    </Card>
+                                )
+                            })}
+                        </Card.Group>
+                        </> : null
+                    }
+                </Layout>
+            </>
+        )
+    }
 }
 export default Gallery
