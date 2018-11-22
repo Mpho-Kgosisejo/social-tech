2412a43cfb709872a572a3ad2c0a811aad3e0b82
Thato MEKWA <tmekwa@e4r8p10.wethinkcode.co.za>
2018-11-22 14:05:45 +0200

    added the index menu component

--- components/Layouts/Features/Index/IndexMenu.js
+++ components/Layouts/Features/Index/IndexMenu.js
@@ -1,5 +1,5 @@
 import Layout from "../../Layout"
-import { Header, Image, Grid } from "semantic-ui-react";
+import { List, Image, Grid, Header } from "semantic-ui-react";
 import React from 'react'
 
 class IndexMenu extends React.Component {
@@ -10,12 +10,72 @@ class IndexMenu extends React.Component {
                     <div className="index-menu-container">
                         <div className="index-menu-row">
                             <Grid columns={2} divided>
-                                <Grid.Column>
-                                    <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <Image src='https://react.semantic-ui.com/images/wireframe/media-paragraph.png' />
-                                </Grid.Column>
+                                <Grid.Row>
+                                    <Grid.Column>
+                                        <List>
+                                            <List.Item>
+                                                <Image size="tiny" avatar src='https://demos.onepagelove.com/html/resto/img/img_square_1.jpg' />
+                                                <List.Content>
+                                                    <Header as="h4">FRIED POTATOES WITH GARLIC</Header>
+                                                    Crab / Potatoes / Rice
+                                                </List.Content>
+                                                </List.Item>
+                                                <List.Item>
+                                                <Image  size="tiny" avatar src='https://demos.onepagelove.com/html/resto/img/img_square_2.jpg' />
+                                                <List.Content>
+                                                <   Header as="h4">SALTED FRIED CHICKEN</Header>
+                                                    Crab / Potatoes / Rice
+                                                </List.Content>
+                                            </List.Item>
+                                            <List.Item>
+                                            <Image  size="tiny" avatar src='https://demos.onepagelove.com/html/resto/img/img_square_3.jpg' />
+                                            <List.Content>
+                                            <Header as="h4">ROAST BEEF (4 STICKS)</Header>
+                                                Crab / Potatoes / Rice
+                                            </List.Content>
+                                            </List.Item>
+                                            <List.Item>
+                                            <Image size="tiny" avatar src='https://demos.onepagelove.com/html/resto/img/img_square_4.jpg' />
+                                            <List.Content>
+                                                <Header as="h4">TUNA ROAST SOURCE</Header>
+                                                Crab / Potatoes / Rice
+                                            </List.Content>
+                                            </List.Item>
+                                        </List>
+                                    </Grid.Column>
+                                    <Grid.Column>
+                                        <List>
+                                            <List.Item>
+                                                <Image size="tiny" avatar src='https://demos.onepagelove.com/html/resto/img/img_square_5.jpg' />
+                                                <List.Content>
+                                                    <Header as="h4">BAKED POTATO PIZZA</Header>
+                                                    Crab / Potatoes / Rice
+                                                </List.Content>
+                                                </List.Item>
+                                                <List.Item>
+                                                <Image size="tiny" avatar src='https://demos.onepagelove.com/html/resto/img/img_square_4.jpg' />
+                                                <List.Content>
+                                                    <Header as="h4">SALTED FRIED CHICKEN</Header>
+                                                    Crab / Potatoes / Rice
+                                                </List.Content>
+                                            </List.Item>
+                                            <List.Item>
+                                            <Image size="tiny" avatar src='https://demos.onepagelove.com/html/resto/img/img_square_3.jpg' />
+                                            <List.Content>
+                                                <Header as="h4">TUNA ROAST SOURCE</Header>
+                                                Crab / Potatoes / Rice
+                                            </List.Content>
+                                            </List.Item>
+                                            <List.Item>
+                                            <Image size="tiny" avatar src='https://demos.onepagelove.com/html/resto/img/img_square_2.jpg' />
+                                            <List.Content>
+                                                <Header as="h4">FRIED POTATOES WITH GARLIC</Header>
+                                                Crab / Potatoes / Rice
+                                            </List.Content>
+                                            </List.Item>
+                                        </List>
+                                    </Grid.Column>
+                                </Grid.Row>
                             </Grid>
                         </div>
                     </div>
