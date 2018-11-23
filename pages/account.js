ec055de307cc476483529b47bffccc6b9adce650
Thato MEKWA <tmekwa@e4r8p10.wethinkcode.co.za>
2018-11-23 11:09:26 +0200

    merge conflicts

--- pages/account.js
+++ pages/account.js
@@ -7,35 +7,13 @@ class account extends React.Component{
 
     render(){
         return(
-        <Layout title="Account" includeContainer={false}>
+          <Layout>
+            <React.Fragment>
+                <ProfileLayout />
+            </React.Fragment>
+          </Layout>
 
-         <Sidebar.Pushable as={Segment}>
-    <Sidebar as={Menu} animation='overlay' icon='labeled' inverted vertical visible width='thin'>
-      <Menu.Item as='a'>
-        <Icon name='home' />
-        Home
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='gamepad' />
-        Games
-      </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='camera' />
-        Channels
-      </Menu.Item>
-    </Sidebar>
-
-    <Sidebar.Pusher>
-      <Segment basic>
-        <Header as='h3'>Application Content</Header>
-        <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
-      </Segment>
-    </Sidebar.Pusher>
-  </Sidebar.Pushable>
-        </Layout>
-
-        )
+          ) 
     }
-
 }
 export default account
