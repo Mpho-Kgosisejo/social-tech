914775e16bea5846996b284c3ff5737430dd6f8c
Thato MEKWA <tmekwa@e4r8p10.wethinkcode.co.za>
2018-11-26 16:42:27 +0200

    added the payment info on my account page

--- components/Layouts/Features/Account/AccountPersonalDetails.js
+++ components/Layouts/Features/Account/AccountPersonalDetails.js
@@ -10,36 +10,36 @@ class AccountPersonalDetails extends React.Component {
                     <Form>
                         <Form.Field>
                             <label>First name:</label>
-                            <input />
+                            <input value="Thato" readOnly/>
                         </Form.Field>
                         <Form.Field>
                             <label>Last name:</label>
-                            <input />
+                            <input value="Mekwa" readOnly/>
                         </Form.Field>
                         <Form.Field>
                             <label>Username:</label>
-                            <input />
+                            <input value="tmekwa" readOnly/>
                         </Form.Field>
                         <Form.Field>
                             <label>Email address:</label>
-                            <input />
+                            <input value="tmekwa@gmail.com" readOnly/>
                         </Form.Field>
                         <Form.Field>
                             <label>Phone:</label>
-                            <input />
+                            <input value="0123456789" readOnly/>
                         </Form.Field>
                         <Form.Field>
                             <label>Occupation:</label>
-                            <input />
+                            <input value="Student" readOnly/>
                         </Form.Field>
                         <Form.Field>
                             <label>Bio:</label>
-                            <textarea />
+                            <textarea value="Pickle Rick oooooooooo motherfuckers im alive and im coming to your school bitch argh and we going to make more seasons bitch in your face wrinkled scrotum bitch from Mars im coming for you hahahahahahah nine for seasons bitch yea nine more and im get that sauce" readOnly/>
                         </Form.Field>
                         <Divider />
                         <Form.Field>
                             <label>Password:</label>
-                            <input />
+                            <input value="************" readOnly/>
                         </Form.Field>
                         <Button type='submit'>Submit</Button>
                     </Form>
