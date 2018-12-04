be9ae97dfc2c1b699ab552b8e3b4e046d1644080
Mpho KGOSISEJO <mkgosise@e4r9p11.wethinkcode.co.za>
2018-12-04 13:47:38 +0200

    added remove_file to multer-handler

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -1,4 +1,8 @@
+import path from "path"
+import fs from "fs"
+
 import UserModel from "../models/User"
+import { removeFile } from "../utils/multerImageHandler";
 
 export const update_info = (req, res) => {
     const {key} = req.auth_data
@@ -29,20 +33,42 @@ export const update_info = (req, res) => {
 export const update_avator = (req, res) => {
     const {key} = req.auth_data
 
-    UserModel.findByIdAndUpdate({_id: key}, {
-        image: req.file.path
-    }, {new: true})
+    UserModel.findById(key)
     .then(user => {
-        res.json({
-            avator: user.image,
-            message: "OK"
+        if (user.image) {
+            // Remove image of: user.image
+            removeFile(user.image, (err) => {
+                if (err){
+                    console.error("RemoveImage: ", err)
+                }else{
+                    console.log("RemoveImage: OK")
+                }
+            })
+        }
+
+        UserModel.findByIdAndUpdate({_id: key}, {
+            image: req.file.path
+        }, {new: true})
+        .then(user => {
+            res.json({
+                avator: user.image,
+                message: "OK"
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                catch: err,
+                error: {
+                    message: "Error updating user [1]"
+                }
+            })
         })
     })
     .catch(err => {
         res.status(500).json({
             catch: err,
             error: {
-                message: "Error updating user"
+                message: "Error updating user [2]"
             }
         })
     })
