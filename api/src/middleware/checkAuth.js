19c9edb31a8228ade3b885dbb004a411415ae416
Mpho KGOSISEJO <mkgosise@e4r9p11.wethinkcode.co.za>
2018-12-04 12:39:59 +0200

    auth feature now has logic

--- api/src/middleware/checkAuth.js
+++ api/src/middleware/checkAuth.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken"
+import UserModel from "../models/User"
 
 const response = (res, extra_message = "") => {
     res.status(401).json({
@@ -9,22 +10,37 @@ const response = (res, extra_message = "") => {
 }
 
 const checkAuth = (req, res, next, isAdmin) => {
-    
     try {
         const token = req.headers.authorization.split(" ")[1] || ""
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decodedToken = jwt.verify(decoded.token, process.env.JWT_SECRET)
 
-        req.auth_data = decoded
+        req.auth_data = decodedToken
 
-        if (isAdmin){
-            if (decoded.isAdmin){
-                next()
-            }else{
-                response(res, " (NOT ADMIN)")         
-            }
-        }else{
-            next()
+        if (!decodedToken.key){
+            response(res)
+            return
         }
+        UserModel.findOne({_id: decodedToken.key, email: decodedToken.email})
+        .then(user => {
+            if (user && (user._id == decodedToken.key)){
+                if (isAdmin){
+                    if (decodedToken.isAdmin){
+                        next()
+                        return
+                    }
+                    response(res, " (NOT ADMIN)")         
+                    return
+                }else{
+                    next()
+                    return
+                }
+            }
+            response(res)
+        })
+        .catch(() => {
+            response(res)
+        })  
     } catch (error) {
        response(res)
     }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import App, { Container } from 'next/app'
 import Config from "react-global-configuration"
+import axios from "axios"
 
 import devConfig from "../src/config/devConfig"
 import prodConfig from "../src/config/prodConfig"
@@ -65,6 +66,8 @@ export default class MyApp extends App {
 
         CartHandler.restore_cart({dispatch: this.state.dispatch})
         if (process.browser){
+            axios.defaults.headers.authorization = `Bearer ${login.token}`
+            
             this.setState({
                 ...this.state,
                 root_loading: false,
