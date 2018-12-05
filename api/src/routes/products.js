2fe7a11cc5efccdb9e05eb3c142f4a09ff39bdaa
Andile MKHUMA <amkhuma@e4r9p9.wethinkcode.co.za>
2018-12-05 17:33:38 +0200

    almost done again

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -59,9 +59,12 @@ router.post("/", multerUpload.single('productImage'), (req, res) => {
 
 
     newProduct.save().then(product => {
-        res.status(200).json({
-            product,
-            message : "successfully added the product"
+        Product.find()
+        .then (products => {
+            res.status(200).json({
+                products,
+                message : "successfully added the product"
+            })
         })
     })
     .catch(err => {
@@ -77,6 +80,15 @@ router.post("/", multerUpload.single('productImage'), (req, res) => {
 router.delete("/", (req, res) => {
     Product.findByIdAndRemove(req.body._id)
     .then (result => {
+        const imageToBeDeleted = req.body.image.split('/')[4]
+        removeFile(`uploads/${imageToBeDeleted}`, (err) => {
+            if (err){
+                console.error("RemoveImage: ", err)
+            }else{
+                console.log("RemoveImage: OK")
+            }
+        })
+
         Product.find()
         .then (data => {
             res.status(200).json({
@@ -97,16 +109,79 @@ router.delete("/", (req, res) => {
 
 router.patch("/", multerUpload.single('productImage'), (req, res) => {
     if (typeof(req.body.oldImagePath) !== 'undefined') {
-        const imageToBeDeleted = req.body.oldImagePath.split('/')[4]
-        console.log('uploads/',imageToBeDeleted)
-        removeFile(`uploads/${imageToBeDeleted}`)
+        console.log(req.body)
+        const {_id, price, available, name, description, menuCategoryId, ingredients, oldImagePath } = req.body
+        const newIngredients = JSON.parse(ingredients)
+        const newImagePath = `${process.env.HOST}/${req.file.path}`
+
+        const updateModel = {
+            price,
+            available,
+            name,
+            image : newImagePath,
+            description,
+            menuCategoryId,
+            ingredients : newIngredients
+        }
+
+        console.log("==============")
+        console.log(updateModel)
+        console.log("==============?", _id)
+
+        Product.findByIdAndUpdate(_id, updateModel, {new : true})
+        .then (resp => {
+
+            Product.find()
+            .then (data => {
+                res.status(200).json({
+                    data,
+                    message : "successfully updated the product."
+                })
+            })
+            .catch (error => {
+                res.status(500).json({
+                    error : {
+                        catch : error,
+                        message : "failed to get updated list of products."
+                    }
+                })
+            })
+
+            const imageToBeDeleted = oldImagePath.split('/')[4]
+            removeFile(`uploads/${imageToBeDeleted}`, (err) => {
+                if (err){
+                    console.error("update image: ", err)
+                }else{
+                    console.log("update image: OK")
+                }
+            })
+
+        })
+        .catch (error => {
+            res.status(500).json({
+                error : {
+                    catch : error,
+                    message : "failed to update the product."
+                }
+            })
+        })
     }
     else {
-        const updateModel = req.body
+        const {_id, price, available, name, description, menuCategoryId, ingredients, image } = req.body
+        // const newIngredients = JSON.parse(ingredients)
 
-        Product.findByIdAndUpdate(req.body._id, updateModel, {new : true})
+        const updateModel = {
+            price,
+            available,
+            name,
+            image ,
+            description,
+            menuCategoryId,
+            ingredients 
+        }
+
+        Product.findByIdAndUpdate(_id, updateModel, {new : true})
         .then (data => {
-            console.log(data)
             Product.find()
             .then (result => {
                 res.status(200).json({
