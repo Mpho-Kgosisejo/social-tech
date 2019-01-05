0f68eee2b373b3e05dd7dc20ff16f92499e50909
Mpho KGOSISEJO <mkgosise@e4r9p11.wethinkcode.co.za>
2019-01-05 17:50:10 +0200

    added comments to APIRequest

--- src/providers/APIRequest.js
+++ src/providers/APIRequest.js
@@ -5,6 +5,12 @@ import mock from "./data/mock"
 
 const API = {
     user: {
+        /**
+         * "signin" is used to authenticate the user.
+         * 
+         * Returns API response of endpoint: [POST, /auth]
+         * @param credentials [Object => {login: {key: [String => "username/email"], value: [String]}, password: [String]}]
+         */
         signin: (credentials) => {
             if (Config.get("api.isMock")){
                 return (mock.login().then(res => res))
@@ -18,6 +24,13 @@ const API = {
                 .catch(err => err.response)
             )
         },
+
+        /**
+         * "signup" is used to register the user.
+         * 
+         * Returns API response of endpoint => [POST, /user]
+         * @param user
+         */
         signup: (user) => {
             if (Config.get("api.isMock")){
                 return (mock.signup().then(res => res))
@@ -32,6 +45,13 @@ const API = {
                 .catch(err => err.response)
             )
         },
+
+        /**
+         * "confirmEmail" is used to confirm user's email after they have registered.
+         * 
+         * Returns API response of endpoint => [POST, /auth/confirmation]
+         * @param token [String]
+         */
         confirmEmail: (token) => {
             if (Config.get("api.isMock")){
                 return (mock.confirmEmail().then(res => res))
@@ -44,6 +64,13 @@ const API = {
                 .catch(err => err.response)
             )
         },
+
+        /**
+         * "requestPasswordChange" is used to request "password cahange", sending a link (via email) to the user so they able to change and also confim if user (of email) exists.
+         * 
+         * Returns API response of endpoint => [POST, /auth/reset-password] (Sends user a link via email)
+         * @param email [String]
+         */
         requestPasswordChange: (email) => {
             if (Config.get("api.isMock")){
                 return (mock.requestPasswordChange().then(res => res))
@@ -54,6 +81,13 @@ const API = {
                 .catch(err => err.response)
             )
         },
+
+        /**
+         * "changePassword" is used to change/reset user's password.
+         * 
+         * Returns API response of endpoint => [POST, /auth/change-password]
+         * @param object [Object] => {token: [String, "token given to user via email"], password: [String, "user's new password"]}
+         */
         changePassword: ({token, password}) => {
             if (Config.get("api.isMock")){
                 return (mock.changePassword().then(res => res))
