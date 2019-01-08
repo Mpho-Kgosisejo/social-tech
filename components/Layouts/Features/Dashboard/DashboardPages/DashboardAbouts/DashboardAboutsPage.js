a3b0441b69935712865ded869658e6a689f8fbcd
Melissa NAIDOO <mnaidoo@e4r8p11.wethinkcode.co.za>
2019-01-08 15:31:53 +0200

    responsiveness (dropdown)in progess

--- components/Layouts/Features/Dashboard/DashboardPages/DashboardAbouts/DashboardAboutsPage.js
+++ components/Layouts/Features/Dashboard/DashboardPages/DashboardAbouts/DashboardAboutsPage.js
@@ -8,6 +8,7 @@ import AboutStory from '../DashboardAbouts/components/abouts_ourstory'
 import AboutContact from '../DashboardAbouts/components/abouts_contactus'
 import AboutFaq from '../DashboardAbouts/components/abouts_faq'
 import AboutsChef from '../DashboardAbouts/components/our_chef';
+import api from '../../../../../../src/providers/APIRequest';
 
 class DashboardAboutsPage extends React.Component {
 
@@ -19,37 +20,73 @@ class DashboardAboutsPage extends React.Component {
       panes: [
         {
           menuItem: 'Our Story Form', render: () => <Tab.Pane>
-            <AboutStory/>
+            <AboutStory />
           </Tab.Pane>
-        
+
         },
         {
-          menuItem: 'Our Chefs Form', render:() => <Tab.Pane>
-            <AboutsChef/>
-          </Tab.Pane>
-        },
-        {
-          menuItem: 'Contact Us Form', render:() => <Tab.Pane>
-            <AboutContact/>
+          menuItem: 'Our Chefs Form', render: () => <Tab.Pane>
+            <AboutsChef />
           </Tab.Pane>
         },
         {
-          menuItem: 'Our FAQs Form', render:() => <Tab.Pane>
-            <AboutFaq/>
+          menuItem: 'Contact Us Form', render: () => <Tab.Pane>
+            <AboutContact />
+          </Tab.Pane>
+        },
+        {
+          menuItem: 'Our FAQs Form', render: () => <Tab.Pane>
+            <AboutFaq />
           </Tab.Pane>
         }
       ]
     }
-    
-    
-
   }
+
+  getData = async ({ index }) => {
+
+    const data = await api.web.about()
+
+    if (data.status === 200) {
+      this.props.dispatch({ type: "ABOUT", payload: { index, ...data.data } })
+      this.setState({ loading: false })
+    }
+    else {
+      this.setState({ loading: false })
+    }
+  }
+
+  componentDidMount() {
+
+    const { tab } = this.props.router.query
+
+    if (tab) {
+      switch (tab) {
+        case "ourstory":
+          this.getData({ index: 0 })
+          break;
+        case "ourchefs":
+          this.getData({ index: 1 })
+          break;
+        case "ourcontacts":
+          this.getData({ index: 2 })
+          break;
+        case "ourfaqs":
+          this.getData({ index: 3 })
+          break;
+      }
+    }
+  }
+
+
+
+
   render() {
-    const { panes} = this.state
-    
+    const { panes } = this.state
+
     return (
       <div className="dashboard-page-container">
-      <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
+        <Tab menu={{ secondary: true, pointing: true }} panes={panes} />
       </div>
     )
   }
