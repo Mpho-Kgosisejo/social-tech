0c8ea70e205e888e3f689a1022fd9ed58bda166c
Andile Mkhuma <lungilekhambule19@gmail.com>
2019-04-02 13:30:45 +0200

    Update Footer.js

--- components/Layouts/Footer.js
+++ components/Layouts/Footer.js
@@ -16,11 +16,11 @@ const Footer = () => (
                         <Grid.Row columns={3}>
                             <Grid.Column className="footer_col_1">
                                 <h3 className="footer_h3">Our address</h3>
-                                <p className="footer_p"> 22 Alnahas Building, 2 AlBahr St, Tanta
+                                <p className="footer_p"> 84 Albertina Sisulu Rd, Johannesburg, 2000
                             <br />
-                                    Al-Gharbia Governorate, Egypt
+                                    Gauteng, Johannesburg
                         </p>
-                                <a className="footer_a" target="_blank" href="https://www.google.com/maps/place/48+Silwood+Rd,+Bramley,+Johannesburg,+2018/@-26.1279852,28.0803513,17z/data=!3m1!4b1!4m5!3m4!1s0x1e950d167db50e61:0x6b58942d39a1bc0!8m2!3d-26.12799!4d28.08254">VIEW ON MAP</a>
+                                <a className="footer_a" target="_blank" href="https://www.google.com/maps/place/WeThinkCode_/@-26.2048491,28.0400679,19.75z/data=!4m13!1m7!3m6!1s0x1e950d167db50e61:0x6b58942d39a1bc0!2s48+Silwood+Rd,+Bramley,+Johannesburg,+2018!3b1!8m2!3d-26.12799!4d28.08254!3m4!1s0x1e950ea2272f1561:0x77a3e32d421bda49!8m2!3d-26.2049382!4d28.040159">VIEW ON MAP</a>
                             </Grid.Column>
                             <Grid.Column className="footer_col_2">
                                 <h3 className="footer_h3">Stay connected</h3>
@@ -32,7 +32,7 @@ const Footer = () => (
                             <Grid.Column className="footer_col_3">
                                 <h3 className="footer_h3">Contact us</h3>
                                 <p className="footer_p">
-                                    Main Email: FreshEats@mailer.com
+                                    Main Email: FreshChew@mailer.com
                         <br />
                                     Phone: 012 345 6789
                         </p>
